3a3ea473395187ac4fd28c104ec5ff4dab42bd87
Olga Lukashenko <lukashenko.93@bk.ru>
2022-05-29 16:08:03 +0800

    Second commit

--- first.test.js
+++ first.test.js
@@ -1,4 +1,6 @@
 const { chromium } = require("playwright");
+const {expect} = require("@playwright/test");
+const { email, password } = require('./user.js');
 
 (async () => {
   const browser = await chromium.launch({
@@ -9,14 +11,16 @@ const { chromium } = require("playwright");
   const page = await browser.newPage();
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.locator('[placeholder="Email"]').click();
-  await page.locator('[placeholder="Email"]').fill("email");
+  await page.locator('[placeholder="Email"]').fill(email);
   await page.locator('[type="password"]').click();
-  await page.locator('[type="password"]').fill("password");
+  await page.locator('[type="password"]').fill(password);
   await page.click("//button[text()='Войти']");
-  
+
   await page.pause();
 
-  await expect(page).toHaveText('Неверный email');
+  await expect("Мои курсы и профессии").toEqual(await page.textContent("h2"));
+  await expect(page.locator("text=Мои курсы и профессии"));
 
   await browser.close();
+  
 })();
--- second.test.js
+++ second.test.js
@@ -1,5 +1,5 @@
 const { chromium } = require("playwright");
-const { email, password } = require('./user.js');
+const {expect} = require("@playwright/test");
 
 (async () => {
   const browser = await chromium.launch({
@@ -10,14 +10,14 @@ const { email, password } = require('./user.js');
   const page = await browser.newPage();
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.locator('[placeholder="Email"]').click();
-  await page.locator('[placeholder="Email"]').fill(email);
+  await page.locator('[placeholder="Email"]').fill("email@mail.com");
   await page.locator('[type="password"]').click();
-  await page.locator('[type="password"]').fill(password);
+  await page.locator('[type="password"]').fill("password");
   await page.click("//button[text()='Войти']");
-
+  
   await page.pause();
 
-  await expect(page.locator('.components-pages-Profile-Programs--title--3JKZ1')).toHaveText('Мои курсы и профессии');
+  await expect(page.locator("text=Вы ввели неправильно логин или пароль"));
 
   await browser.close();
 })();
